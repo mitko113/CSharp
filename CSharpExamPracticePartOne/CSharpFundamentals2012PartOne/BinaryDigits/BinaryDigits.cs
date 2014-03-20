b6e0d32cc0e80216bfa5a7c159ad62d6c9408adf
mitko113 <mitko113@abv.bg>
2014-03-20 23:45:05 +0200

    .init

---
+++ CSharpExamPracticePartOne/CSharpFundamentals2012PartOne/BinaryDigits/BinaryDigits.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryDigits
+{
+    class BinaryDigits
+    {
+
+        static int BitCounter(ulong n, short counter) 
+        {
+            counter = 0;
+            while (n > 0)
+            {
+                if (0 == (n & 1))
+                {
+                    counter++;
+                }               
+                n = n >> 1;
+            }
+            return counter;                    
+        }
+
+        static int BitCounter1(ulong n, short counter)
+        {
+            counter = 0;
+            while (n != 0)
+            {
+                if (1 == (n & 1))
+                {
+                    counter++;
+                }
+                n = n >> 1;               
+            }
+            return counter;
+        }
+
+        static void Main()
+        {
+            byte bitValue = byte.Parse(Console.ReadLine());
+            short n = short.Parse(Console.ReadLine());
+            ulong[] number = new ulong[n];
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = ulong.Parse(Console.ReadLine());
+            }
+
+            int result = 0;
+
+            if (bitValue == 0)
+            {
+                for (int i = 0; i < number.Length; i++)
+                {
+                    result = BitCounter(number[i], counter);
+                    Console.WriteLine(result);
+                }
+            }
+            else if (bitValue == 1)
+            {
+                for (int i = 0; i < number.Length; i++)
+                {
+                    result = BitCounter1(number[i], counter);
+                    Console.WriteLine(result);
+                }
+            }
+        }
+          public static short counter { get; set; }
+    }
+}
---
+++ CSharpExamPracticePartOne/CSharpFundamentals2012PartOne/ForestRoad/ForestRoad.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ForestRoad
+{
+    class ForestRoad
+    {
+        static void Main()
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = 2 * width - 1;
+            int counter = 0;
+
+            for (int row = 1; row <= width; row++)
+            {
+                counter++;
+                for (int col = 1; col <= width; col++)
+                {
+                    if (counter == col)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            for (int row = 1; row < width; row++)
+            {
+                counter--;
+                for (int col = 1; col <= width; col++)
+                {
+                    if (counter == col)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
