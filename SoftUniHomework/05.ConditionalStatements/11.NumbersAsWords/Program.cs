0205cc3bf90584e5556ffcd3ddd2ee97505823fb
mitko113 <mitko113@abv.bg>
2014-03-25 19:19:42 +0200

    .init

---
+++ SoftUniHomework/05.ConditionalStatements/11.NumbersAsWords/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersAsWords
+{
+    class NumbersAsWords
+    {
+
+
+        static void PrintNumbersFrom1UpTo9(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    Console.Write("one ");
+                    break;
+                case 2:
+                    Console.Write("two ");
+                    break;
+                case 3:
+                    Console.Write("three ");
+                    break;
+                case 4:
+                    Console.Write("four ");
+                    break;
+                case 5:
+                    Console.Write("five ");
+                    break;
+                case 6:
+                    Console.Write("six ");
+                    break;
+                case 7:
+                    Console.Write("seven ");
+                    break;
+                case 8:
+                    Console.Write("eight ");
+                    break;
+                case 9:
+                    Console.Write("nine ");
+                    break;
+            }
+        }
+
+        static void PrintNumbersFrom10UpTo19(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    Console.Write("ten ");
+                    break;
+                case 1:
+                    Console.Write("eleven ");
+                    break;
+                case 2:
+                    Console.Write("twelve ");
+                    break;
+                case 3:
+                    Console.Write("thirteen ");
+                    break;
+                case 4:
+                    Console.Write("fourteen ");
+                    break;
+                case 5:
+                    Console.Write("fifteen ");
+                    break;
+                case 6:
+                    Console.Write("sixteen ");
+                    break;
+                case 7:
+                    Console.Write("seventeen ");
+                    break;
+                case 8:
+                    Console.Write("eighteen ");
+                    break;
+                case 9:
+                    Console.Write("nineteen ");
+                    break;
+            }
+        }
+
+        static void PrintNumbersFrom20UpTo90(int number)
+        {
+            switch (number)
+            {
+                case 2:
+                    Console.Write("twenty ");
+                    break;
+                case 3:
+                    Console.Write("thirty ");
+                    break;
+                case 4:
+                    Console.Write("forty ");
+                    break;
+                case 5:
+                    Console.Write("fifty ");
+                    break;
+                case 6:
+                    Console.Write("sixty ");
+                    break;
+                case 7:
+                    Console.Write("seventy ");
+                    break;
+                case 8:
+                    Console.Write("eighty ");
+                    break;
+                case 9:
+                    Console.Write("ninety ");
+                    break;
+
+            }
+        }
+
+
+
+
+        static void Main()
+        {
+            Console.WriteLine("Enter a number form 0 to 99 ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0 || num > 999)
+            {
+                Console.WriteLine("Wrong input !Number must be in range [0...99]");
+            }
+            if (num == 0)
+            {
+                Console.WriteLine("Zero");
+            }
+
+            if (num> 0)
+            {
+                int hundred = num / 100;
+                switch (hundred)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        Console.Write("One hundred ");
+                        break;
+                    case 2:
+                        Console.Write("Two hundred ");
+                        break;
+                    case 3:
+                        Console.Write("Three hundred ");
+                        break;
+                    case 4:
+                        Console.Write("Four hundred ");
+                        break;
+                    case 5:
+                        Console.Write("Five hundred ");
+                        break;
+                    case 6:
+                        Console.Write("Six hundred ");
+                        break;
+                    case 7:
+                        Console.Write("Seven hundred ");
+                        break;
+                    case 8:
+                        Console.Write("Eight hundred ");
+                        break;
+                    case 9:
+                        Console.Write("Nine hundred ");
+                        break;
+
+                }
+            }
+
+            if ((num % 100) > 0)
+            {
+                if (num >= 100)
+                {
+                    Console.Write("and ");
+                }     
+
+                int tens = (num % 100) / 10;
+                if (tens == 1)
+                {
+                    PrintNumbersFrom10UpTo19((num % 100) % 10);
+                    Console.WriteLine();
+                    Console.ReadLine();
+                    return;
+                }
+                else if (tens > 1)
+                {
+                    PrintNumbersFrom20UpTo90(tens);
+                }
+                int remainder = (num % 100) % 10;
+                if (remainder > 0)
+                {
+                    PrintNumbersFrom1UpTo9(remainder);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SoftUniHomework/05.ConditionalStatements/12.SubsetSums/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.SubsetSums
+{
+    class Program
+    {
+        static void Main()
+        {
+            long wantedSum = 0;          
+            Console.WriteLine("Enter the number of elements:");
+            long numberOfElements = long.Parse(Console.ReadLine());
+            long[] elements = new long[numberOfElements];
+            int counter = 0;
+            string subset = "";
+            for (int i = 0; i < elements.Length; i++)
+            {
+                Console.WriteLine("Enter element № {0}", i + 1);
+                elements[i] = long.Parse(Console.ReadLine());
+            }
+            int maxSubsets = (int)Math.Pow(2, numberOfElements) - 1;
+            for (int i = 1; i <= maxSubsets; i++)
+            {
+                subset = "";
+                long checkingSum = 0;
+                for (int j = 0; j <= numberOfElements; j++)
+                {
+                    int mask = 1 << j;
+                    int nAndMask = i & mask;
+                    int bit = nAndMask >> j;
+                    if (bit == 1)
+                    {
+                        checkingSum = checkingSum + elements[j];
+                        subset = subset + " " + elements[j];
+                    }
+                }
+                if (checkingSum == wantedSum)
+                {
+                    Console.WriteLine("Number of subest that have the sum of {0}", wantedSum);
+                    counter++;
+                    Console.WriteLine("This subset has a sum of {0} : {1} ", wantedSum, subset);
+                }
+            }
+        }
+    }
+}
